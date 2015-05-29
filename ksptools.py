b87d6dd3dc12aa6811b7bcaebd022aeb5f816287
mueslo <johannesfalke@googlemail.com>
2015-05-29 18:12:24 +0200

    fixed incorrect dv/mp plots

--- ksptools.py
+++ ksptools.py
@@ -6,6 +6,11 @@ from tools import OrderedDefaultDict
 import numpy as np
 import bisect
 
+from collections import namedtuple
+import os
+
+g0 = 9.81
+
 ConfigNode = lambda: OrderedDefaultDict(list)
 
 class KerbalConfig(object):
@@ -94,3 +99,110 @@ def pc_func_from_params(parameters):
         return a + b*x + c*x**2 + d*x**3
     
     return np.vectorize(func)
+
+Engine = namedtuple('Engine', ['name', 'mass', 'fvac', 'isp', 'isp_func'])
+
+def get_engines(ksp_dir = "."):
+    """ Returns list of type Engine """
+    engine_cfgs = []
+    for root, dirs, files in os.walk(ksp_dir+'/GameData/Squad/Parts/Engine'):
+        #dirs[:] = [d for d in dirs if "liquidEngine" in d]
+
+        for cfg in (f for f in files if ".cfg" in f):
+            engine_cfgs.append(KerbalConfig(root+"/"+cfg))
+
+    engines = []
+    for e_cfg in engine_cfgs:
+        part = e_cfg.PART[-1]
+        d = {}
+        name = part["title"][-1]
+
+        repls = (('Liquid Fuel Engine', 'LFE'),
+                 ('Liquid Engine', 'LE'),
+                 ('Atomic Rocket Motor', 'ARM'),
+                 ('Solid Fuel Booster', 'SFB'),
+                 ('Electric Propulsion System', 'EPS')
+        name = reduce(lambda a, kv: a.replace(*kv), repls, name)
+        name = name.strip()
+
+        d["name"] = name
+        d["mass"] = float(part["mass"][-1])
+
+        for module in part["MODULE"]:       
+            atmosphere_curve = module.get("atmosphereCurve")
+            if atmosphere_curve:
+                try:
+                    curvekeys = atmosphere_curve[-1].get("key")
+                    d["fvac"] = float(module["maxThrust"][-1])
+                    atm_params = [tuple(float(x) for x in k.split()) for k in curvekeys]
+                    d["isp"] = atm_params
+                    d["isp_func"] = pc_func_from_params(atm_params)
+                except (NotImplementedError, KeyError) as e:
+                    print "skipping",name,"due to",type(e)
+                else:
+                    engines.append(Engine(**d))
+                finally:
+                    break
+
+    engines.sort(key=lambda e: e.mass)
+    
+    return engines
+
+#calculates number of engines needed to attain a certain TWR,
+# with infinitely divisible, 0-dry-mass fuel tanks
+def n_e(engine, (Dv, M_p), min_twr=0., pressure=0., max_engine_count=np.inf):
+    if (min_twr==0): #if there's no minimum twr, a single engine is the best in terms of mass and therefore fuel efficiency
+        return np.ones(np.shape(M_p)) #shape(M_p) *should* == shape(Dv)
+    else:
+        isp = engine.isp_func(pressure)
+        thrust = engine.fvac * isp/engine.isp_func(0)
+        u = isp*g0
+        
+        #calculate engine count
+        # via tsiolkovsky rocket eq, see kerbalplot.ipynb
+        num = np.ceil(M_p/(thrust/(min_twr*g0*np.exp(Dv/u))-engine.mass))
+        
+        #using ceiling here isn't actually correct, but only first guess. This is because
+        # the fractional engine we rounded up from also only has fraction weight. For large m_p/dv this shouldn't
+        # matter too much, but for light crafts this might be very wrong
+        
+        forbidden = num>max_engine_count #mask array where engine count is greater than largest allowed
+        num[forbidden] = np.inf #set forbidden
+     
+        forbidden = num<1 #mask array where engines needed is less than one (forbidden)
+        num[forbidden] = np.inf #set forbidden
+        return num
+
+# TODO:
+    # max fuel (e.g. SRB)
+    # min fuel (e.g. Mammoth, SRB)
+    # fuel tank weight
+def get_optimal_engine_configuration(engines, dv, mp, pressure=0., min_twr=0., max_engine_count=np.inf):
+    pass
+
+    #configuration space: fuel tank type&count, engine type&count
+
+
+def compute(engines, (Dv, M_p), **kwargs):
+    pressure = kwargs['pressure']
+    min_twr = kwargs['min_twr']
+    max_engine_count = kwargs['max_engine_count']
+    #print "Computing..."
+    #TODO iterate explicitly so that n is not calculated twice
+    #TODO multiprocessing?
+
+    # number of engines needed
+    n = np.array([n_e(e, (Dv, M_p), min_twr=min_twr, pressure=pressure, max_engine_count=max_engine_count) for e in engines])
+    
+    # total mass
+    M = [(n[i]*e.mass+M_p)*np.exp(Dv/(g0*e.isp_func(pressure))) for i,e in enumerate(engines)]
+    #M = np.array([(n_e(e,M_p,Dv,min_twr=min_twr, pressure=pressure, max_engine_count=max_engine_count)*e.mass+M_p)*np.exp(Dv/(g0*e.isp_func(pressure))) for e in engines])
+
+    # index of engine with configuration of least mass
+    I = np.argmin(M,axis=0)     
+
+    b = np.sum(np.isinf(M), axis=0)==len(engines) #mask array where all engines were inf
+    I[b] = len(engines) #set the index to len(engines) (out of bounds)
+
+    #print "Done."
+    return n, M, I
